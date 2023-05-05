133526e06e19df9648ceee329ed585a07e6a1f97
Carlos Scala <carloss@awlatam.com>
2023-05-04 22:04:10 -0300

    snow commit

--- components/footer.js
+++ components/footer.js
@@ -8,12 +8,12 @@ class Footer extends HTMLElement {
         <footer>
             <div class="cs-footer">
                 <div class="cs-footer-social-links">
-                    <a href="#"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-spotify.svg" alt="logo sitio"></a>
-                    <a href="#"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-facebook-1.svg" alt="logo sitio"></a>
-                    <a href="#"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-pinterest.svg" alt="logo sitio"></a>
-                    <a href="#"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-instagram-1.svg" alt="logo sitio"></a>
-                    <a href="#"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-youtube.svg" alt="logo sitio"></a>
-                    <a href="#"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-twitter.svg" alt="logo sitio"></a>
+                    <a href="#" target="_blank"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-spotify.svg" alt="logo sitio"></a>
+                    <a href="#" target="_blank"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-facebook-1.svg" alt="logo sitio"></a>
+                    <a href="#" target="_blank"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-pinterest.svg" alt="logo sitio"></a>
+                    <a href="#" target="_blank"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-instagram-1.svg" alt="logo sitio"></a>
+                    <a href="#" target="_blank"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-youtube.svg" alt="logo sitio"></a>
+                    <a href="#" target="_blank"><img class="cs-footer-social-icon" src="../assets/icons/socialMedia/icon-twitter.svg" alt="logo sitio"></a>
                 </div>
                 <div class="cs-footer-faq-list">
                     <a href="#">Aviso de Privacidad</a>
