0ebf5dd2da655b3d4b03ef0297ac0112a494d604
Carlos Scala <carloss@awlatam.com>
2023-05-06 22:38:02 -0300

    correccion planeta

--- js/productosTabla.js
+++ js/productosTabla.js
@@ -24,16 +24,7 @@ let productos = [
     {  Image: '../assets/products/tabla2.webp',Nombre: "Tabla Nitro Prime", Cantidad: 25, Tipo: "Standard", Precio:181990},
     {  Image: '../assets/products/tabla3.webp',Nombre: "Roxy Breeze Snowboard 2023", Cantidad: 25, Tipo: "Premium", Precio:400990},
 ];
-{/*
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div> 
-*/}
+
 function generateTable(container, data) {
 
     for (let element of data) {
